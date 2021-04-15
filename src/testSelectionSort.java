86d07aa8b02cfd373b415b85c8fb4b078520f26d
Dmitry <dmitryk@umich.edu>
2021-04-15 13:11:40 -0400

    Positive and Neagative new branch

--- src/testSelectionSort.java
+++ src/testSelectionSort.java
@@ -10,98 +10,98 @@ public class testSelectionSort {
 		testNegative();
 		testMixed();
 		testDuplicates();
-		}    
-	public testSelectionSort() {    
-		
-	}    
-	public void testPositive(){        
-		int[] arr = new int[5];        
-		arr[0] = 8;        
-		arr[1] = 9;        
-		arr[2] = 7;        
-		arr[3] = 10;        
-		arr[4] = 2;     
-		
+		}
+	public testSelectionSort() {
+
+	}
+	public void testPositive(){
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
+
 		SelectionSort temp1 = new SelectionSort();
 		int[] maxValue = temp1.basicSelectionSort(arr);
-		
-		int[] Sortedarr = new int[5];     
-		Sortedarr[0] = 2;     
-		Sortedarr[1] = 7;     
-		Sortedarr[2] = 8;       
-		Sortedarr[3] = 9;      
-		Sortedarr[4] = 10;    
-		
-	
-		//assertArrayEquals(maxValue, Sortedarr);
-		
-		/** add tests to check for this unit test **/    
-		}    public void testNegative(){     
-			/** Test data contains negative values only **/    
-			int[] arr = new int[5];        
-			arr[0] = -8;        
-			arr[1] = -9;        
-			arr[2] = -7;        
-			arr[3] = -10;        
-			arr[4] = -2;     
-			
+
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = 2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = 8;
+		Sortedarr[3] = 9;
+		Sortedarr[4] = 10;
+
+
+		assertArrayEquals(maxValue, Sortedarr);
+
+		/** add tests to check for this unit test **/
+		}    public void testNegative(){
+			/** Test data contains negative values only **/
+			int[] arr = new int[5];
+			arr[0] = -8;
+			arr[1] = -9;
+			arr[2] = -7;
+			arr[3] = -10;
+			arr[4] = -2;
+
 			SelectionSort temp1 = new SelectionSort();
 			int[] maxValue = temp1.basicSelectionSort(arr);
-			
-			int[] Sortedarr = new int[5];     
-			Sortedarr[0] = -10;     
-			Sortedarr[1] = -9;     
-			Sortedarr[2] = -8;       
-			Sortedarr[3] = -7;      
-			Sortedarr[4] = -2;    
-			
-			//assertArrayEquals(maxValue, Sortedarr);
-			}    
+
+			int[] Sortedarr = new int[5];
+			Sortedarr[0] = -10;
+			Sortedarr[1] = -9;
+			Sortedarr[2] = -8;
+			Sortedarr[3] = -7;
+			Sortedarr[4] = -2;
+
+			assertArrayEquals(maxValue, Sortedarr);
+			}
 		public void testMixed(){
-			
-			
-        /** Test data contains with both positive, negative and zeros **/    
-			int[] arr = new int[5];        
-			arr[0] = -8;        
-			arr[1] = 9;        
-			arr[2] = -7;        
-			arr[3] = 0;        
-			arr[4] = -2;     
-			
+
+
+        /** Test data contains with both positive, negative and zeros **/
+			int[] arr = new int[5];
+			arr[0] = -8;
+			arr[1] = 9;
+			arr[2] = -7;
+			arr[3] = 0;
+			arr[4] = -2;
+
 			SelectionSort temp1 = new SelectionSort();
 			int[] maxValue = temp1.basicSelectionSort(arr);
-			
-			int[] Sortedarr = new int[5];     
-			Sortedarr[0] = -8;     
-			Sortedarr[1] = -7;     
-			Sortedarr[2] = -2;       
-			Sortedarr[3] = 0;      
-			Sortedarr[4] = 9;    
-			
+
+			int[] Sortedarr = new int[5];
+			Sortedarr[0] = -8;
+			Sortedarr[1] = -7;
+			Sortedarr[2] = -2;
+			Sortedarr[3] = 0;
+			Sortedarr[4] = 9;
+
 		//	assertArrayEquals(maxValue, Sortedarr);
-			
-			
-			
-			}    public void testDuplicates(){       
-				/** Test data contains duplicates **/ 
-				
-				int[] arr = new int[5];        
-				arr[0] = -8;        
-				arr[1] = 9;        
-				arr[2] = 0;        
-				arr[3] = 0;        
-				arr[4] = -8;     
-				
+
+
+
+			}    public void testDuplicates(){
+				/** Test data contains duplicates **/
+
+				int[] arr = new int[5];
+				arr[0] = -8;
+				arr[1] = 9;
+				arr[2] = 0;
+				arr[3] = 0;
+				arr[4] = -8;
+
 				SelectionSort temp1 = new SelectionSort();
 				int[] maxValue = temp1.basicSelectionSort(arr);
-				
-				int[] Sortedarr = new int[5];     
-				Sortedarr[0] = -8;     
-				Sortedarr[1] = -8;     
-				Sortedarr[2] = 0;       
-				Sortedarr[3] = 0;      
-				Sortedarr[4] = 9;    
-				
-				assertArrayEquals(maxValue, Sortedarr);
+
+				int[] Sortedarr = new int[5];
+				Sortedarr[0] = -8;
+				Sortedarr[1] = -8;
+				Sortedarr[2] = 0;
+				Sortedarr[3] = 0;
+				Sortedarr[4] = 9;
+
+				//assertArrayEquals(maxValue, Sortedarr);
 				}
 			}
